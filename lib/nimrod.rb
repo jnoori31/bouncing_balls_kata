147147bc19304b3002b355576f209bd7383ac4c6
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2021-11-24 17:02:47 +0000

    rspec working on lib file

---
+++ lib/nimrod.rb
@@ -0,0 +1,9 @@
+class Nimrod
+  def nimrod(h, bounce, window)
+    return -1 if (h <= 0) || (bounce <= 0) || (bounce >= 1) || (window >= h)
+
+    counter = 0
+    counter += (h *= bounce) > window ? 2 : 1 while h >= window
+    counter
+  end
+end
--- spec/nimrod_spec.rb
+++ spec/nimrod_spec.rb
@@ -1,40 +1,39 @@
-require './nimrod'
 require 'nimrod'
 
 describe Nimrod do
   it '1- should return 3' do
-    expect(subject.bouncing_ball(3, 0.66, 1.5)).to eq(3)
+    expect(subject.nimrod(3, 0.66, 1.5)).to eq(3)
   end
 
   it '2- should return -1' do
-    expect(subject.bouncing_ball(0, 0.66, 1.5)).to eq(-1)
+    expect(subject.nimrod(0, 0.66, 1.5)).to eq(-1)
   end
 
   it '3- should return 15' do
-    expect(subject.bouncing_ball(30, 0.66, 1.5)).to eq(15)
+    expect(subject.nimrod(30, 0.66, 1.5)).to eq(15)
   end
 
   it '4- should return 21' do
-    expect(subject.bouncing_ball(30, 0.75, 1.5)).to eq(21)
+    expect(subject.nimrod(30, 0.75, 1.5)).to eq(21)
   end
 
   it '5- should return 3' do
-    expect(subject.bouncing_ball(30, 0.4, 10)).to eq(3)
+    expect(subject.nimrod(30, 0.4, 10)).to eq(3)
   end
 
   it '6- should return -1' do
-    expect(subject.bouncing_ball(40, 1, 10)).to eq(-1)
+    expect(subject.nimrod(40, 1, 10)).to eq(-1)
   end
 
   it '7- should return -1' do
-    expect(subject.bouncing_ball(-5, 0.66, 1.5)).to eq(-1)
+    expect(subject.nimrod(-5, 0.66, 1.5)).to eq(-1)
   end
 
   it '8- should return -1' do
-    expect(subject.bouncing_ball(3, 0.66, 3)).to eq(-1)
+    expect(subject.nimrod(3, 0.66, 3)).to eq(-1)
   end
 
   it '9- should return 2' do
-    expect(subject.bouncing_ball(6, 0.5, 3)).to eq(2)
+    expect(subject.nimrod(6, 0.5, 3)).to eq(2)
   end
 end
