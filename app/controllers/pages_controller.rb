5985381b0229ba42feb3c4713c44002354032786
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2021-12-17 11:17:12 +0000

    backend notes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,3 +2,13 @@ class PagesController < ApplicationController
 	def home
 	end
 end
+
+require nimrod
+
+def calculate
+	random = 3 random variables
+	computer_answer = nimrod(random)
+
+	@user_answer(x)
+	checked the @computer_answer
+	end
