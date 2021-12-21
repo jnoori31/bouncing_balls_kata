a2b42582d332542427b74bff3c2730c96d7bd35f
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2021-12-21 12:16:02 +0000

    annotations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,3 +12,20 @@ def playGame
 	@user_answer(x)
 	checked the @computer_answer
 	end
+
+
+# wallet = 100
+# stop_playing = ""
+
+# window height is less than full height
+# ball is dropped from full height
+# It will bounce passed the window how many times?
+# full height = known
+# window height = known
+# bounce = User_guess
+
+# display question with both height & window_height revealed
+# Ask user_guess for bounce
+# user_bounce === @computer_bounce
+# update socore +2 or -1
+# Update games played, display current score with number games played
